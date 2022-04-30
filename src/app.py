b2ffbb99a9278387f0953cedcc5e3ddc2161addb
gt1974 <gt294@cornell.edu>
2022-04-30 17:24:26 -0400

    added create user endpoint

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from db import db
 import json
 from flask import Flask
 from flask import request
+from db import User
 
 app = Flask(__name__)
 db_filename = "travel_log.db"
@@ -20,3 +21,18 @@ def success_response(data, code=200):
 
 def failure_response(message, code=400):
     return json.dumps({"error": message}), code
+
+# Routes
+@app.route("/users/", methods=["POST"])
+def create_user():
+    """
+    Endpoint for creating a new user
+    """
+    body = json.loads(request.data)
+    name = body.get("name")
+    if name is None:
+        return failure_response("need name")
+    new_user = User(name = name)
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
