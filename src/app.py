8beb6d3f533886ce951456a6ab177a72a496f7a4
gt1974 <gt294@cornell.edu>
2022-05-06 02:20:17 -0400

    implemented log in

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from turtle import up
 from db import Post, db
 import json
 from flask import Flask
@@ -5,6 +6,7 @@ from flask import request
 from db import User
 from db import Place
 from db import Category
+import users_dao
 
 app = Flask(__name__)
 db_filename = "travel_log.db"
@@ -24,6 +26,18 @@ def success_response(data, code=200):
 def failure_response(message, code=400):
     return json.dumps({"error": message}), code
 
+def extract_token(request):
+    """
+    Helper function that extracts the token from the header of a request
+    """
+    auth_header = request.headers.get("Authorization")
+    if auth_header is None:
+        return False, json.dumps({"Missing authorization header"})
+
+    bearer_token = auth_header.replace("Bearer", "").strip()
+
+    return True, bearer_token
+
 # USER Routes
 @app.route("/")
 @app.route("/users/")
@@ -36,19 +50,94 @@ def get_users():
         users.append(user.serialize()) 
     return success_response({"users": [u.serialize() for u in User.query.all()]})
 
-@app.route("/users/", methods=["POST"])
+@app.route("/login/", methods=["POST"])
+def login():
+    """
+    Endpoint for logging in a user
+    """
+    body = json.loads(request.data)
+    email = body.get("email")
+    password = body.get("password")
+    if email is None or password is None:
+        return failure_response("Missing email or password", 400)
+    was_successful, user = users_dao.verify_credentials(email, password)
+
+    if not was_successful:
+        return failure_response("Incorrect username or password", 401)
+    
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }, 201
+    )
+@app.route("/secret/", methods=["GET"])
+def secret_message():
+    """
+    Endpoint for verifying a session token and returning a secret message
+
+    In your project, you will use the same logic for any endpoint that needs 
+    authentication
+    """
+    was_successful, session_token = extract_token(request)
+    if not was_successful:
+        return session_token
+    
+    user= users_dao.get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return failure_response("invalid session token")
+    return success_response({"message": "you have implemented sessions"})
+
+@app.route("/register/", methods=["POST"])
 def create_user():
     """
-    Endpoint for creating a new user
+    Endpoint for registering a new user
     """
     body = json.loads(request.data)
     name = body.get("name")
+    email = body.get("email")
+    password = body.get("password")
     if name is None:
         return failure_response("need name")
-    new_user = User(name = name)
+    if email is None or password is None:
+        return failure_response("Missing email or password")
+    
+    was_successful, new_user = users_dao.create_user(name, email, password)
+    if not was_successful:
+        return failure_response("user already exists")
+
     db.session.add(new_user)
     db.session.commit()
-    return success_response(new_user.serialize(), 201)
+    return success_response(
+        {
+            "session_token": new_user.session_token,
+            "session_expiration": str(new_user.session_expiration),
+            "update_token": new_user.update_token
+        }, 201
+        
+    )
+@app.route("/session/", methods=["POST"])
+def update_session():
+    """
+    Endpoint for updating a user's session
+    """
+    was_successful, update_token = extract_token(request)
+
+    if not was_successful:
+        return update_token
+    try:
+        user = users_dao.renew_session(update_token)
+    except Exception as e:
+        return failure_response(f"Invalid update token: {str(e)}")
+    
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }, 201
+    )
 
 @app.route("/users/<int:user_id>/")
 def get_user(user_id):
@@ -192,4 +281,4 @@ def assign_category(post_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
